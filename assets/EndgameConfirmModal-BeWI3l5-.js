35865df8111ecafb36ed1c9ee75dd9fba6bf09c9
KepenJ <kepenj@gmail.com>
2026-06-03 11:48:59 +0800

    Updates

--- assets/EndgameConfirmModal-BeWI3l5-.js
+++ assets/EndgameConfirmModal-BeWI3l5-.js
@@ -1,4 +1,4 @@
-import{c as n,u as r,j as e,t as l,X as o}from"./index-BzHW8vV-.js";/**
+import{c as n,u as r,j as e,t as l,X as o}from"./index-BurPpKWg.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
