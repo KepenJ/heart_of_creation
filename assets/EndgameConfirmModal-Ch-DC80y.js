f8c55b54e5ef27f4b2e5fef122e35e40781a50a7
KepenJ <kepenj@gmail.com>
2026-06-08 16:28:22 +0800

    Updates

--- assets/EndgameConfirmModal-Ch-DC80y.js
+++ assets/EndgameConfirmModal-Ch-DC80y.js
@@ -1,4 +1,4 @@
-import{c as n,u as a,j as e,t as l,X as o}from"./index-DzSiV-ZL.js";/**
+import{c as n,u as a,j as e,t as l,X as o}from"./index-Du7-dTW5.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
