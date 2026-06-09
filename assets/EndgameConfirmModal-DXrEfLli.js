8f4af159e6650df93e8759abe62557bc5981f5ba
KepenJ <kepenj@gmail.com>
2026-06-09 10:04:01 +0800

    Updates

--- assets/EndgameConfirmModal-DXrEfLli.js
+++ assets/EndgameConfirmModal-DXrEfLli.js
@@ -1,4 +1,4 @@
-import{c as n,u as a,j as e,t as l,X as o}from"./index-CyN7HeYe.js";/**
+import{c as n,u as a,j as e,t as l,X as o}from"./index-Djwl79I7.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
