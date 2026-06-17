1082b9570ff037e5cbca251aa8ffb0d054ac9531
KepenJ <kepenj@gmail.com>
2026-06-18 04:00:10 +0800

    Updates

--- assets/EndgameConfirmModal-Cs39SD1_.js
+++ assets/EndgameConfirmModal-Cs39SD1_.js
@@ -1,4 +1,4 @@
-import{c as n,u as t,j as e,t as d,X as o}from"./index-n12QdB7b.js";/**
+import{c as n,u as t,j as e,t as d,X as o}from"./index-CIYOYtpN.js";/**
  * @license lucide-react v0.460.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/web-CHlzEFho.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-n12QdB7b.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
